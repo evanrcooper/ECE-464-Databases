0db490c3dad0d4c7ad1e998ddfa1bdbdbe71b453
Evan Rosenfeld <erosenfeld@imagikids.com>
2025-02-24 15:21:24 -0500

    starting note taking and fixed sqlite syntax errors
    Signed-off-by: Evan Rosenfeld <erosenfeld@imagikids.com>

--- Project/WebApp/db_init.sql
+++ Project/WebApp/db_init.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
 	username TEXT NOT NULL,
 	display_name TEXT NOT NULL,
 	encrypted_passkey TEXT NOT NULL,
-	active BOOLEAN NOT NULL DEFAULT 1,
+	active BOOLEAN NOT NULL DEFAULT 1
 );
 
 -- only usernames for active accounts require uniqueness
@@ -14,23 +14,23 @@ CREATE UNIQUE INDEX IF NOT EXISTS unique_active_usernames
 
 -- contains extra information about the users
 CREATE TABLE IF NOT EXISTS user_heuristics (
-    uuid INTEGER PRIMARY KEY FOREIGN KEY users(uuid),
+    uuid INTEGER PRIMARY KEY REFERENCES users(uuid),
     articles_read_count INTEGER NOT NULL DEFAULT 0,
     articles_liked_count INTEGER NOT NULL DEFAULT 0,
     articles_disliked_count INTEGER NOT NULL DEFAULT 0,
     CHECK (articles_read_count >= 0),
     CHECK (articles_liked_count >= 0),
-    CHECK (articles_disliked_count >= 0),
+    CHECK (articles_disliked_count >= 0)
 );
 
 -- contains entries for users reading, liking, and disliking articles
 CREATE TABLE IF NOT EXISTS article_interactions (
-    uuid INTEGER FOREIGN KEY users(uuid),
-    article_id INTEGER FOREIGN KEY articles(article_id),
-    (article_id, genre_id) PRIMARY KEY,
-    like BOOLEAN NOT NULL DEFAULT 0,
-    dislike BOOLEAN NOT NULL DEFAULT 0,
+    uuid INTEGER NOT NULL REFERENCES users(uuid),
+    article_id INTEGER NOT NULL REFERENCES articles(article_id),
+    liked BOOLEAN NOT NULL DEFAULT 0,
+    disliked BOOLEAN NOT NULL DEFAULT 0,
     read BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY (uuid, article_id)
 );
 
 -- main article table
@@ -41,30 +41,30 @@ CREATE TABLE IF NOT EXISTS articles (
 	author TEXT NULL,
 	publish_date DATETIME NULL,
 	submitted_date DATETIME NOT NULL,
-	submitter_uuid INTEGER FOREIGN KEY users(UUID),
+	submitter_uuid INTEGER REFERENCES users(UUID),
 	path TEXT NOT NULL,
     like_count INTEGER NOT NULL DEFAULT 0,
-    CHECK (like_count >= 0),
+    CHECK (like_count >= 0)
 );
 
 -- holds information about the articles like text summaries and vector encodings
 CREATE TABLE IF NOT EXISTS article_heuristics (
-    article_id INTEGER PRIMARY KEY FOREIGN KEY articles(article_id),
+    article_id INTEGER PRIMARY KEY REFERENCES articles(article_id),
     summary_path TEXT NULL DEFAULT NULL,
-    vector BLOB NULL DEFAULT NULL,
+    vector BLOB NULL DEFAULT NULL
 );
 
 -- indexes all genres (aka keywords)
-CREATE TABLE genres (
+CREATE TABLE IF NOT EXISTS genres (
     genre_id /* AUTOINCREMENT */ INTEGER PRIMARY KEY, -- https://www.sqlite.org/autoinc.html
-    genre_name TEXT NULL DEFAULT NULL,
+    genre_name TEXT NULL DEFAULT NULL
 );
 
 -- contains entries for if an article is associated with a genre
-CREATE TABLE article_genres (
-	article_id INTEGER NOT NULL FOREIGN KEY articles(article_id),
-	genre_id INTEGER NOT NULL FOREIGN KEY genres(genre_id),
-	(article_id, genre_id) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS article_genres (
+	article_id INTEGER NOT NULL REFERENCES articles(article_id),
+	genre_id INTEGER NOT NULL REFERENCES genres(genre_id),
+	PRIMARY KEY (article_id, genre_id)
 );
 
 -- DIR STRUCTURE
--- Project/WebApp/main.py
+++ Project/WebApp/main.py
@@ -1,1 +1,18 @@
-print('main.py')
+import numpy as np
+import pandas as pd
+import requests
+import os
+import sqlite3 as sq3
+import pathlib as pl
+
+database_path: str = '/evanr/ece464.sqlite3'
+database_init_path: str = '/app/db_init.sql'
+
+def main() -> None:
+    conn: sq3.Connection = sq3.connect(database_path)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
