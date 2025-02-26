3cd04c2d6ddae98a600b9c41933445652ef18b13
flof <evanrose99@gmail.com>
2025-02-26 18:46:52 -0500

    idk

--- Project/WebApp/NewsAPI/article_json_newsapi.py
+++ Project/WebApp/NewsAPI/article_json_newsapi.py
@@ -18,17 +18,17 @@ qs = {
 }
 
 API_KEY: str = os.getenv('NewsAPI_KEY')
-PAGE = 1
-
-print(API_KEY)
+NUM_PAGES = 4
 
 for q in qs:
-    url: str = f'https://newsapi.org/v2/everything?q={q}&apiKey={API_KEY}&page={PAGE}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        with open(f'./newsapi_articles_{q}.json', 'w') as f:
-            json.dump(data, f, indent=4)
-    else:
-        print(f'Failed to fetch data, status code: {response.status_code}')
-        exit(response.status_code)
+    for page in range(NUM_PAGES):
+        url: str = f'https://newsapi.org/v2/everything?q={q}&apiKey={API_KEY}&page={page+1}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            with open(f'./Data/newsapi_articles_{q}_pg{page+1}.json', 'w') as f:
+                json.dump(data, f, indent=4)
+        else:
+            print(f'Failed to fetch data, status code: {response.status_code}')
+            print(url)
+            exit(response.status_code)
